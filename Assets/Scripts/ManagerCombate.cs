0ec23436fde85702ec4de135882d39f716ccaaa7
Dreikenn <31456192+Dreikenn@users.noreply.github.com>
2017-11-04 19:33:53 -0300

    master cod XD
    pelea por turno no se aplica el movmiento

---
+++ Assets/Scripts/ManagerCombate.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerCombate : MonoBehaviour {
+
+    public List<Moverse> Soldados;
+
+    public Button prefab;
+    public static ManagerCombate singleton;
+    public Text TextoEstado;
+    public Transform panel;
+
+    void Awake ()
+    {
+        if(singleton != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        singleton = this;
+		
+	}
+
+    public void Actualizarinterface()
+    {
+        TextoEstado.text = "";
+        foreach (var soldado in Soldados)
+        {
+            if(soldado.SigueVivo)
+            {
+                TextoEstado.text += "<color=" + (soldado.Aliado ? "green" : "red") + ">" +
+                soldado.nombre + "HP: " + soldado.vida + "/100 PM: " + soldado.PM + "/100.</color>\n";
+            }
+            
+        }
+    }
+    void Start()
+    {
+        Actualizarinterface();
+        StartCoroutine("Bucle");
+    }
+    List<Button> poolbotones = new List<Button>();
+    IEnumerator Bucle()
+    {
+        while(true)
+        {
+            IEnumerator c = null;
+
+            foreach(var Soldado in Soldados)
+            {
+                if(Soldado.SigueVivo)
+                {
+                    if(Soldado.Aliado)
+                    {
+                        foreach(var accion in Soldado.Acciones)
+                        {
+                            Button b = null;
+                            for (int i = 0; i < poolbotones.Count; i++)
+                            {
+                                if (!poolbotones[i].gameObject.activeInHierarchy)
+                                {
+                                    b = poolbotones[i];
+                                 
+                                }
+                            }
+
+                            b = Instantiate(prefab, panel);
+
+                            b.transform.position = Vector3.zero;
+                            b.transform.localScale = Vector3.one;
+
+                            poolbotones.Add(b);
+
+                            b.gameObject.SetActive(true);
+                            b.onClick.RemoveAllListeners();
+                            b.GetComponentInChildren<Text>().text = accion.nombre;
+                            if (Soldado.PM < accion.CostoPM)
+                            {
+                                b.interactable = false;
+                            }
+                            else
+                            {
+                                b.interactable = true;
+                                b.onClick.AddListener(() =>
+                                {
+                                    for (int j = 0; j < poolbotones.Count; j++)
+                                    {
+                                        poolbotones[j].gameObject.SetActive(false);
+                                    }
+
+                                    c = Soldado.EjecutarAccion(
+                                         accion,
+                                         Soldados[Random.Range(0, Soldados.Count)].transform);
+                                });
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        c = Soldado.EjecutarAccion(
+                        Soldado.Acciones[Random.Range(0, Soldado.Acciones.Count)],
+                        Soldados[Random.Range(0, Soldados.Count)].transform);
+                    }
+                    while(c == null)
+                    {
+                        yield return null;
+                    }
+
+                    yield return StartCoroutine(c);
+                    yield return new WaitForSeconds(1);
+
+                }
+
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Actualizarinterface();
+    }
+}
---
+++ Assets/Scripts/Moverse.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+//nota     transform.LookAt(objetivo)  sirve para evitar que rote al mirar y lo haga al instante
+
+[System.Serializable]
+public struct Accion
+{
+    public string nombre;
+    public bool Estatico;
+    public bool SoyObjetivo;
+
+    //controla la vida de forma sencilla, mensaje
+    public string mensaje;
+    // puede ser cantidad de vida que quita o agrega
+    public int Argumento;
+    //seria mejor agregar las animaciones desde este script
+    public string animacionTrigger;
+    //puntos de movimiento
+    
+
+    public float CostoPM;
+
+}
+public class Moverse : MonoBehaviour {
+
+    public List<Accion> Acciones;
+
+    public string nombre;
+    public int vida;
+    public float PM;
+    public bool Aliado;
+    public bool SigueVivo = true;
+
+    public float Distancia;
+
+
+
+    void CambiarVida(int cant)
+    {
+        vida += cant;
+        
+    }
+    void CambiarPuntosDeAccion(float cant)
+    {
+        PM += cant;
+        
+    }
+    NavMeshAgent NV;
+    ManagerCombate mc;
+    Animator anim;
+    Vector3 Objetivo;
+
+    void Start () 
+	{
+        //Declaro que voy a usar NavMesh
+        anim = GetComponent<Animator>();
+		NV = GetComponent<NavMeshAgent> ();
+	}
+
+    public IEnumerator EjecutarAccion(Accion accion, Transform objetivo)
+    {
+        CambiarPuntosDeAccion(-accion.CostoPM);
+
+        if(accion.SoyObjetivo)
+        {
+            objetivo = transform;
+        }
+        if(accion.Estatico)
+        {
+            anim.SetTrigger(accion.animacionTrigger);
+            objetivo.SendMessage(accion.mensaje, accion.Argumento);
+        }
+        else
+        {
+            Vector3 PosInicial = transform.position;
+
+            NV.SetDestination(objetivo.position);
+
+            while (Vector3.Distance(transform.position, objetivo.position) > 2)
+                yield return null;
+            anim.SetTrigger(accion.animacionTrigger);
+            objetivo.SendMessage(accion.mensaje, accion.Argumento);
+
+        }
+    }
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//Si se hace click se guarda la posicion del click
+		if (Input.GetMouseButtonDown (0))
+		{
+            moverme();
+
+            
+        
+        }
+	}
+
+   
+
+    void moverme()
+    {
+        Ray Rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Posicion;
+
+        if (Physics.Raycast(Rayo, out Posicion))
+        {
+            //Posicion.point nos da el lugar exacto donde el rayo choco.
+            Objetivo = Posicion.point;
+            Distancia = Vector3.Distance(Posicion.point, transform.position);
+
+            Debug.DrawLine(transform.position, Objetivo, Color.green);
+
+
+           
+            SetearPosicion();
+            PM = Distancia - PM;
+
+        }
+        
+        
+        
+       
+    }
+
+	void SetearPosicion()
+	{
+        if (Aliado == false)
+        {
+            return;
+        }
+        if (PM <= 0)
+        {
+            return;
+        }
+        else
+        {
+            NV.SetDestination(Objetivo);
+        }
+	}
+}
