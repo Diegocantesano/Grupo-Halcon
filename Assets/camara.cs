78e4c25d065727b41b122e90ecfd2ccd0da6a831
diegocantesano <diegocantesano@gmail.com>
2017-11-19 21:16:22 -0300

    nuevo mapa
    agregado nuevo mapa

---
+++ Assets/camara.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara : MonoBehaviour {
+
+	private Transform myTransform;
+	public float TeclaPositiva;
+	public float TeclaNegativa;
+	void Start () {
+		myTransform = GetComponent <Transform>();
+	}
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W))
+			myTransform.Translate (new Vector3 (0, TeclaPositiva, 0));
+			
+		if (Input.GetKey (KeyCode.S))
+			myTransform.Translate (new Vector3 (0, TeclaNegativa, 0));
+
+		/*if (Input.GetKey (KeyCode.A))
+			myTransform.Translate (new Vector3 (TeclaNegativa, 0, 0));
+
+		if (Input.GetKey (KeyCode.D))
+			myTransform.Translate (new Vector3 (TeclaPositiva, 0, 0));
+*/	}
+}
