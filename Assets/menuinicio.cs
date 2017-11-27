b997dd4b9bf691a6d847a33a70d6cb5f152a609c
fedejavitaroco <31699354+fedejavitaroco@users.noreply.github.com>
2017-11-27 00:08:45 -0300

    menues(falta aplicar las direcciones)

---
+++ Assets/menuinicio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuinicio : MonoBehaviour {
+
+    public void comienzo()
+    {
+        Application.LoadLevel("Policias");
+
+    }
+}
