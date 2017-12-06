e99d6cc7a49a7482dc4830bf78f9440ac18e32b7
diegocantesano <diegocantesano@gmail.com>
2017-12-06 11:49:17 -0300

    Edicion etapa 1
    - endings
    - imagenes
    - interfaz acomodad
    - bugs de codigos

---
+++ Assets/RegresarMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegresarMenu : MonoBehaviour {
+
+	public void inicio()
+	{
+		Application.LoadLevel("menudeinicio");
+
+	}
+}
