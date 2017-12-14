d2b445a1ba0536dce8c676f18aee18c46a617968
LuzGarcia97 <31461472+LuzGarcia97@users.noreply.github.com>
2017-12-14 17:33:09 -0300

    Enemigo y Policias
    Enemigos funcionales. Policias a seleccion

---
+++ Assets/Scripts/SelectorDePersonajes.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class SelectorDePersonajes : MonoBehaviour
+{
+
+    public GameObject Poli1;
+    public GameObject Poli2;
+    public GameObject Poli3;
+
+    public bool showGUI = false;
+
+	void Start ()
+    {
+        Poli2.SetActive(false);
+        Poli3.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            showGUI = true;
+        }
+	}
+
+    private void OnGUI()
+    {
+        if (showGUI==true)
+        {
+            if (GUI.Button(new Rect(10, 10, 50, 50), "1"))
+          
+            {
+                Poli1.SetActive(true);
+                Poli2.SetActive(false);
+                Poli3.SetActive(false);
+            }
+
+            if(GUI.Button(new Rect(60, 60, 50, 50), "2"))
+
+
+            {
+                Poli1.SetActive(false);
+                Poli2.SetActive(true);
+                Poli3.SetActive(false);
+            }
+
+            if (GUI.Button(new Rect(10, 110, 50, 50), "3"))
+
+
+            {
+                Poli1.SetActive(false);
+                Poli2.SetActive(false);
+                Poli3.SetActive(true);
+            }
+
+        }
+    }
+}
